6e7872a572b9713fc9c37ad1c6ddc67982bf48b8
Muhammed-Yusa-Yigit <147321793+Muhammed-Yusa-Yigit@users.noreply.github.com>
2023-10-09 00:32:59 +0300

    Initial Commit

---
+++ Assets/Scenes/FinishLine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    public GameObject finishLine;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject)
+        {
+            Debug.Log("Çizgi");
+        }
+    }
+}
+
+
+
